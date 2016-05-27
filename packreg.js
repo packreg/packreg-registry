5977b22d06d772c1777cf7a91744c4546ee9dbde
Ben Mann <mann@blanko.de>
2016-05-27 11:18:44 +0200

    Stub CLI
    - add cli to start the application
    - removes need for app.js later

---
+++ packreg.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+var packreg = require("commander"),
+    chalk = require("chalk");
+
+packreg.version("1.0.0").usage("<command>");
+
+
+// TODO: add options to run specific collectors (bower/npm/...)
+packreg.command("collect")
+.description("Starts collecting packages and pushes them into the analyzing queue.")
+.action(function() {
+  console.log(chalk.green("running collectors..."));
+  // run collectors
+});
+
+
+packreg.command("queue")
+.description("Output curent stats of the analyzing queue.")
+.option("-c, --clear", "Clear the analyzing queue of all remaining jobs.")
+.action(function(cmd, options) {
+  var options = process.argv.slice(3)[0];
+  
+  if(options == "--clear" || options == "-c"){
+    // run clear command
+  }
+
+  // run queue stats
+});
+
+
+packreg.parse(process.argv);
+if(!packreg.args.length) {
+    packreg.help();
+}
